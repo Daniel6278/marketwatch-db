2c3c211afb271a96d4b227e7e832d7956f4caa5a
williamqin123 <willqin123@gmail.com>
2025-11-11 18:27:44 -0600

    view tables

--- backend/api/routers/admin_actions.py
+++ backend/api/routers/admin_actions.py
@@ -8,6 +8,9 @@ from starlette.responses import Response
 
 from ..internal.setup_db import setup_db, db_fill_starter_data
 from ..internal import auth
+from ..dependencies import DB_CONNECT_CONFIG
+
+import json, pymysql
 
 router = APIRouter()
 
@@ -35,5 +38,48 @@ async def insert_static_data_into_db(
 
 
 @router.get("/table/{table_name}", tags=["admin"])
-async def view_table(table_name: str):
-    return table_name
+async def view_table(
+    table_name: str,
+    credentials: Annotated[HTTPBasicCredentials, Depends(auth.security)],
+):
+    ADMIN_AUTHORIZED_TABLES_NAMES = [
+        "Alert",
+        "User",
+        "Ticker",
+        "Portfolio",
+        "PriceHistory",
+        "holdings",
+        "AuditLog",
+    ]  # do not remove! this prevents SQL injection attacks
+    if auth.verify_admin_authentication(credentials.username, credentials.password):
+        if table_name in ADMIN_AUTHORIZED_TABLES_NAMES:
+            with pymysql.connect(**DB_CONNECT_CONFIG) as conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(
+                        """SELECT
+                            COLUMN_NAME,
+                            DATA_TYPE
+                        FROM
+                            INFORMATION_SCHEMA.COLUMNS
+                        WHERE
+                            TABLE_SCHEMA = %s AND TABLE_NAME = %s
+                            AND NOT (TABLE_NAME = 'User' AND (COLUMN_NAME = 'password_hash' OR COLUMN_NAME = 'email'));
+                        """,
+                        (
+                            DB_CONNECT_CONFIG["database"],
+                            table_name,
+                        ),
+                    )
+                    columns_infos = []
+                    reflection_result = cursor.fetchall()
+                    for column_info in reflection_result:
+                        columns_infos.append(column_info)
+
+                    sql = f"SELECT {','.join(f'`{col['COLUMN_NAME']}`' for col in columns_infos)} FROM {table_name};"
+                    cursor.execute(sql)
+                    results = (
+                        cursor.fetchall()
+                    )  # Fetches all results as a list of tuples
+                return json.dumps({"columns": columns_infos, "rows": results}, indent=4)
+
+    raise auth.UNAUTHORIZED_RESPONSE
